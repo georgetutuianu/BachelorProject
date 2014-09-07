091524338ec2dfa2cf28216a0bd35b8be4701636
tzutz92 <tzutz92@12a22f67-d3fa-481e-94f3-2ef16a794385>
2014-09-07 18:01:23 +0000

    Added a fist page and designed my download page.

--- trunk/application/modules/default/controllers/IndexController.php
+++ trunk/application/modules/default/controllers/IndexController.php
@@ -12,7 +12,10 @@ class IndexController extends Utility_Controller_AbstractController
      */
     public function indexAction()
     {
+        $multimediaService = new Service_Multimedia();
+        $downloadList = $multimediaService->getLatestDownloadedVideos();
         
+        $this->view->downloadLinkList = $downloadList;
     }
     
     public function downloadRequestAction()
@@ -20,20 +23,35 @@ class IndexController extends Utility_Controller_AbstractController
         $this->_checkLoggedUser();
         
         $downloadData = $this->getAllParams();
+        $retryRequest = $this->_getParam('entryId');
         
         $downloadForm = new Form_DownloadForm();
         if ($this->getRequest()->isPost() && $downloadForm->isValid($downloadData)) {
             $multimediaService = new Service_Multimedia();
             
             $downloadLink = $downloadData['DownloadLink'];
-            $multimediaService->addDownloadRequest($downloadLink);
+            try {
+                $multimediaService->addDownloadRequest($downloadLink, $retryRequest);
+                
+                $this->_addFlashMessage(
+                    'The download request have been successfully saved!',
+                    BundlePhu_View_Helper_DisplayFlashMessages::SUCCESS_MESSAGE
+                );
+                $this->_redirect('/default/index/download-list');
+            } catch (Exception $exception) {
+                $this->_addFlashMessage(
+                    'The download request has failed. Please try again!',
+                    BundlePhu_View_Helper_DisplayFlashMessages::ERROR_MESSAGE
+                );
+            }
+        }
+        if ($retryRequest) {
             $this->_addFlashMessage(
-                'The download request have been successfully saved!',
-                BundlePhu_View_Helper_DisplayFlashMessages::SUCCESS_MESSAGE
+                'The download request has failed. Please try again!',
+                BundlePhu_View_Helper_DisplayFlashMessages::ERROR_MESSAGE
             );
             $this->_redirect('/default/index/download-list');
         }
-        
         $this->view->downloadForm = $downloadForm;
     }
     

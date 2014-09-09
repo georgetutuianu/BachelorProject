ba717dde04fd13cf29df3a3b434a0171604c7c45
tzutz92 <tzutz92@12a22f67-d3fa-481e-94f3-2ef16a794385>
2014-09-09 20:25:49 +0000

    Solved blobs problem

--- trunk/application/modules/default/controllers/IndexController.php
+++ trunk/application/modules/default/controllers/IndexController.php
@@ -10,37 +10,12 @@ class IndexController extends Utility_Controller_AbstractController
     public function azureAction()
     {
         set_time_limit(0);
-        require 'WindowsAzure\WindowsAzure.php';
-        $connectionString = 'DefaultEndpointsProtocol=https;AccountName=licenta;AccountKey=kIldSIWX1maxG1xj+yw+SgBk+9DN6/oexbu+PwiwINIX6eySp4GMVXPrYDSDWon2mAdluWEThF/rmvMwKKuA4g==';
-        $blobRestProxy = WindowsAzure\Common\ServicesBuilder::getInstance()->createBlobService($connectionString);
         
-        $createContainerOptions = new WindowsAzure\Blob\Models\CreateContainerOptions(); 
-        $createContainerOptions->setPublicAccess(
-            WindowsAzure\Blob\Models\PublicAccessType::CONTAINER_AND_BLOBS
-        );
+        $multimediaService = new Service_Multimedia();
         
-//        $createContainerOptions->addMetaData("id", "value");
-//        $createContainerOptions->addMetaData("key2", "value2");
-        
-        try {
-            // Create container.
-//            $blobRestProxy->createContainer("audios", $createContainerOptions);
-        
-            
-            $blobName = 'testblob';
-            $filename = 'C:\Users\George\SkyDrive\Proiecte\Licenta\Aplicatie\data\downloads\new.txt';
-            $fileContent = file_get_contents($filename);
-            
-            $blobRestProxy->createBlockBlob('audios', $blobName, $fileContent);
-        }
-        catch(ServiceException $e){
-            // Handle exception based on error codes and messages.
-            // Error codes and messages are here: 
-            // http://msdn.microsoft.com/en-us/library/windowsazure/dd179439.aspx
-            $code = $e->getCode();
-            $error_message = $e->getMessage();
-            echo $code.": ".$error_message."<br />";
-        }
+        $cronId = md5(time());
+        $cronId = 'ee54e0dad89dfa5d8816a48e9e0d9cca';
+        $multimediaService->collectGarbage($cronId);
         
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
